161c41b0adc274fe7be550d0c000d437dc949564
meshariSalhouli <mesharialhouli12@gmail.com>
2024-11-03 13:52:08 +0300

    task 4

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,9 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  if (n % 2 === 1) {
+    return true;
+  } else return false;
 }
 
 /**
@@ -33,7 +35,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return ~~(n / 2);
 }
 
 /**
@@ -47,7 +49,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 === 0) {
+    return n * 2;
+  } else if (n % 2 !== 0) {
+    return n * n;
+  }
 }
 
 module.exports = {
